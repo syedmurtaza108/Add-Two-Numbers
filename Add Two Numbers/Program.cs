e3f2a42fd1ae80cc363463575d6f587660db9742
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-18 08:42:34 +0500

    Add project files.

---
+++ Add Two Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
